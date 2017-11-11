2ec9684fc589657703591c7916c290481f3b3f98
bonnichiwa <bonnie.pham@ryerson.ca>
2017-11-11 22:10:07 +0400

    created actions, reducers and store

---
+++ actions/index.js
@@ -0,0 +1,30 @@
+/**
+ * Actions types
+ */
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+/**
+ * Other constants
+ */
+export const VisibilityFilters = {
+	SHOW_ALL: 'SHOW_ALL',
+	SHOW_COMPLETED: 'SHOW_COMPLETED',
+	SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/**
+ * Action creators 
+ */
+export function addTodo(text) {
+	return { type: ADD_TODO, text }
+}
+
+export function toggleToDo(index) {
+	return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+	return { type: SET_VISIBILITY_FILTER, filter: filter}
+}
